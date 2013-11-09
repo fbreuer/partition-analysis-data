7850e40dbb5bd251d9f8cffc7cf5f0e055971070
zaf <zafeirakopoulos@gmail.com>
2013-11-09 19:00:50 +0100

    add validity for maple

--- example/methods/3/validity.py
+++ example/methods/3/validity.py
@@ -1,4 +1,4 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 def correct(output,answer):
     from sage.all import *
@@ -6,12 +6,16 @@ def correct(output,answer):
     F=FractionField(R)
     output_answer=""
     for line_index in range(len(output)):
-        if "InputForm=" in output[line_index]:
-            output_answer=output_answer+output[line_index].split("=")[1]
+        if "writestat(terminal,r,\"END OF RATFUN\");" in output[line_index]:
+            for line in range(len(line_index,output)):
+                if ", \"END OF RATFUN\"" in output[line]:
+                    output_answer=output_answer+output[line_index].split("=")[1]
             for output_index in range(line_index+1,len(output)):
-                output_answer=output_answer+output[output_index].rstrip('\r\n')
-                if output[output_index].strip()=="":
-                    break
+                output_answer=output_answer+sum(output[line_index:line])
+                output_answer.trim()
+                print output_answer
+                break
+
     a=F(sage_eval(output_answer, locals=F.gens_dict()))
     b=F(sage_eval(answer[1], locals=F.gens_dict()))
     print a==b
